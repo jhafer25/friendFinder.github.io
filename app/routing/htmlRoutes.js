37357f6617b017f00d731677286ff7a4dba890a0
James Hafer <jhafer25@gmail.com>
2018-02-18 15:01:37 -0700

    Update favicon

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -9,7 +9,7 @@ module.exports = function(app) {
 		res.sendFile(path.join(__dirname, '../public/home.html'));
 	});
 
-	app.use('/home', function(req, res){
+	app.get('/', function(req, res){
 		res.sendFile(path.join(__dirname + '../public/home.html'));
 	});
 }
