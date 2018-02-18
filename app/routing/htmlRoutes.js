cbcdea277cdb2906f81f558ef4a1fee186555f93
James Hafer <jhafer25@gmail.com>
2018-02-18 14:57:33 -0700

    Update Paths and HTML

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -9,7 +9,7 @@ module.exports = function(app) {
 		res.sendFile(path.join(__dirname, '../public/home.html'));
 	});
 
-	app.use(function(req, res){
+	app.use('/home', function(req, res){
 		res.sendFile(path.join(__dirname + '../public/home.html'));
 	});
 }
